cff682a6aa10804eea84c2f948168b8d079a7b88
Teruteru <teruterubouzu1024@gmail.com>
2019-03-02 01:24:35 +0900

    Change search algorithm
    Update to same algorithm as new Java version

--- src/SlimeHunter.c
+++ src/SlimeHunter.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <time.h>
 #include "random.h"
 #include "mcSlimeChunkOracle.h"
 
@@ -26,39 +27,54 @@ int main(int argc, char* argv[]){
 
   printf("Initial Seed : %"PRId64"\n", rndSeed);
 
-  int64_t x, xMin = -313, xMax = 312;
-  int64_t z, zMin = -313, zMax = 312;
+  int64_t chunkX, xMin = -313, xMax = 312, x;
+  int64_t chunkZ, zMin = -313, zMax = 312, z;
   int64_t countRangeX = 4;
   int64_t countRangeZ = 4;
-  int64_t minSlimeChunks = 14;
+  int64_t minSlimeChunks = 13;
   uint64_t i = 0;
   int32_t slimeChunkCount = 0;
   int32_t chunkCount = 0;
   long exX = 0;
   long exZ = 0;
-  for(i = 0; i < 100000ULL; i++){
+  clock_t start = clock();
+  int64_t searchSeeds = 10000ULL;
+  for(i = 0; i < searchSeeds; i++){
     currentSeed = rndSeed++;
     setMCSeed(&seed, currentSeed);
 
-    for(x = xMin; x < xMax; x++){
-      for(z = zMin; z < zMax; z++){
-        if(isSlimeChunkXZ(&seed, x, z)){
-          slimeChunkCount = 0;
-          chunkCount = 0;
-          for(exX = 0; exX < countRangeX; exX++) {
-            for(exZ = 0; exZ < countRangeZ; exZ++) {
-              chunkCount++;
-              if(isSlimeChunkXZ(&seed, x + exX, z + exZ)){
-                slimeChunkCount++;
+    for(chunkZ = zMin; chunkZ < zMax; chunkZ += 2){
+      for(chunkX = xMin; chunkX < xMax; chunkX += 2){
+        if(isSlimeChunkXZ(&seed, chunkX + 2, chunkZ) && isSlimeChunkXZ(&seed, chunkX + 2, chunkZ + 2)){
+          if(isSlimeChunkXZ(&seed, chunkX, chunkZ) && isSlimeChunkXZ(&seed, chunkX, chunkZ + 2)){
+            for(z = -1; z < 1; z ++){
+              for(x = -1; x < 1; x++){
+                slimeChunkCount = 4;
+                chunkCount = 4;
+                for(exX = 0; exX < countRangeX; exX++) {
+                  for(exZ = 0; exZ < countRangeZ; exZ++) {
+                    if((z + exZ) % 2 != 0 || (x + exX) % 2 != 0){
+                      chunkCount++;
+                      if(isSlimeChunkXZ(&seed, chunkX + exX + x, chunkZ + exZ + z)){
+                        slimeChunkCount++;
+                      }
+                    }
+                  }
+                }
+                if(slimeChunkCount >= minSlimeChunks){
+                  printf("'%"PRId64",(%"PRId64",%"PRId64")(%"PRId64",%"PRId64"),(%"PRId32",%"PRId32")\n", currentSeed, chunkX, chunkZ, chunkX * 16, chunkZ * 16, slimeChunkCount, chunkCount);
+                }
               }
             }
           }
-          if(slimeChunkCount >= minSlimeChunks){
-            printf("'%"PRId64",%"PRId64",%"PRId64",%"PRId32",%"PRId32"\n", currentSeed, x * 16, z * 16, slimeChunkCount, chunkCount);
-          }
+        } else {
+          chunkX += 2;
         }
       }
     }
   }
+  clock_t finish = clock();
+  double seconds = ((double)(finish - start)/ CLOCKS_PER_SEC);
+  printf("%"PRId64"seeds, %.2lfseeds/s, %.2lfs\n", searchSeeds, searchSeeds/seconds, seconds);
 }
 
