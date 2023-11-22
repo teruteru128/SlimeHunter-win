3540494b700ca28b9358a09ad54e8c18f502605c
Teruteru <teruterubouzu1024@gmail.com>
2023-11-22 21:44:21 +0900

    Update test

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../SlimeHunterMT/rnd.h"
-#include "../SlimeHunterMT/slimeHunter.hpp"
+#include "rnd.h"
+#include "slimeHunter.hpp"
+#include <bitset>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -29,6 +30,35 @@ namespace UnitTest1
 			Assert::IsTrue(isSlimeChunk(263622805221400L, 130, -45));
 			Assert::IsTrue(isSlimeChunk(263622805221400L, 131, -45));
 			Assert::IsTrue(isSlimeChunk(263622805221400L, 132, -45));
+		}
+
+		TEST_METHOD(TestMethod2) {
+			using std::bitset;
+			bitset<625 * 625>* set = new bitset<625 * 625>();
+			int z = 0;
+			int x = 0;
+			for (z = 0; z < 625; z++) {
+				for (x = 0; x < 625; x++) {
+					set->set(z * 625 + x, (bool)isSlimeChunk(263622805221400L, x - 312, z - 312));
+				}
+			}
+			Assert::IsTrue(set->test((-48 + 312) * 625 + (129 + 312)));
+			Assert::IsTrue(set->test((-48 + 312) * 625 + (130 + 312)));
+			Assert::IsTrue(set->test((-48 + 312) * 625 + (131 + 312)));
+			Assert::IsTrue(set->test((-48 + 312) * 625 + (132 + 312)));
+			Assert::IsTrue(set->test((-47 + 312) * 625 + (129 + 312)));
+			Assert::IsTrue(set->test((-47 + 312) * 625 + (130 + 312)));
+			Assert::IsTrue(set->test((-47 + 312) * 625 + (131 + 312)));
+			Assert::IsTrue(set->test((-47 + 312) * 625 + (132 + 312)));
+			Assert::IsTrue(set->test((-46 + 312) * 625 + (129 + 312)));
+			Assert::IsTrue(set->test((-46 + 312) * 625 + (130 + 312)));
+			Assert::IsTrue(set->test((-46 + 312) * 625 + (131 + 312)));
+			Assert::IsTrue(set->test((-46 + 312) * 625 + (132 + 312)));
+			Assert::IsTrue(set->test((-45 + 312) * 625 + (129 + 312)));
+			Assert::IsTrue(set->test((-45 + 312) * 625 + (130 + 312)));
+			Assert::IsTrue(set->test((-45 + 312) * 625 + (131 + 312)));
+			Assert::IsTrue(set->test((-45 + 312) * 625 + (132 + 312)));
+			delete set;
 		}
 	};
 }
