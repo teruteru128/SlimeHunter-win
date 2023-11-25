9193887ddad0b796ed3905bf038127882dfb5902
Teruteru <teruterubouzu1024@gmail.com>
2023-11-25 21:24:22 +0900

    include文の修正

--- SlimeHunterMT/mcSlimeChunkOracle.h
+++ SlimeHunterMT/mcSlimeChunkOracle.h
@@ -1,13 +1,15 @@
 
+#pragma once
 #ifndef MC_SLIME_CHUNK_ORACLE_H
 #define MC_SLIME_CHUNK_ORACLE_H
+
+#include "internal_random.h"
+#include "rnd.h"
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include "internal_random.h"
-#include "rnd.h"
-#include <stdbool.h>
 
 #ifdef __cplusplus
 }
